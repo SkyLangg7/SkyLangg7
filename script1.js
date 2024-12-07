5dff0e95c11af4acacb16491812c40cfbf53fd7a
SkyLangg7 <154592963+SkyLangg7@users.noreply.github.com>
2024-12-07 20:59:06 +0700

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,31 @@
+// // percobaan
+
+// const tahunIni = new Date().getFullYear()
+// let tahunLahir = prompt("Masukkan Tahun Lahir Anda")
+// let usia = tahunIni - tahunLahir
+
+// alert(`Usia anda adalah ${usia} tahun`)
+
+// // Tugs 1
+
+// let saldoAwal = Number(prompt("Masukkan Saldo Awal Anda"))
+// let saldoTambahan = Number(prompt("Masukkan Saldo Tambahan Anda"))
+// let saldoAkhir = saldoAwal + saldoTambahan
+
+// alert(`Saldo Akhir Anda sebanyak Rp.${saldoAkhir}`)
+
+// Tugas 2
+
+// let hari = new Date().getDay()
+// const namaHari = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"]
+// const hariIni = namaHari[hari]
+
+// let tanggal = new Date().getDate()
+
+// let bulan = new Date().getMonth()
+// const namaBulan = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"]
+// const bulanIni = namaBulan[bulan]
+
+// let tahun = new Date().getFullYear()
+
+// alert(`Hari ini ${hariIni} ${tanggal} ${bulanIni} ${tahun}`)
